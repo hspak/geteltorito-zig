320497b45a1581c34e7f5d2854e5e68b1914387a
Hong Shick Pak <hong@hspak.com>
2025-04-27 21:28:04 -0700

    Update to zig 0.13

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
         .name = "geteltorio",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
